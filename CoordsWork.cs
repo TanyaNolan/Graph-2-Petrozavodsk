ec5bbfb803f7a5ec9ab5c9f1f64cd049ec317d63
TanyaNolan <36508555+TanyaNolan@users.noreply.github.com>
2018-04-10 21:04:34 +0300

    C# code

---
+++ CoordsWork.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphs2
+{
+    class CoordsWork
+    {
+        private static readonly double R_MAJOR = 6378137.0;
+        private static readonly double R_MINOR = 6356752.3142;
+        private static readonly double RATIO = R_MINOR / R_MAJOR;
+        private static readonly double ECCENT = Math.Sqrt(1.0 - (RATIO * RATIO));
+        private static readonly double COM = 0.5 * ECCENT;
+        private static readonly double DEG2RAD = Math.PI / 180.0;
+
+        private static double DegToRad(double deg)
+        {
+            return deg * DEG2RAD;
+        }
+
+        public static double lonToX(double lon)
+        {
+            return R_MAJOR * DegToRad(lon) * 0.1;
+        }
+
+        public static double latToY(double lat)
+        {
+            lat = Math.Min(89.5, Math.Max(lat, -89.5));
+            double phi = DegToRad(lat);
+            double sinphi = Math.Sin(phi);
+            double con = ECCENT * sinphi;
+            con = Math.Pow(((1.0 - con) / (1.0 + con)), COM);
+            double ts = Math.Tan(0.5 * ((Math.PI * 0.5) - phi)) / con;
+            return 0 - R_MAJOR * Math.Log(ts) * 0.1;
+        }
+    }
+}
